9ce398a4fa5f85c5248b8b0666da3eba41df808c
10067142 <longyuli@juban.com>
2019-04-15 15:41:16 +0800

    add rule  and passworld

--- public/js/home.js
+++ public/js/home.js
@@ -3,30 +3,37 @@ new Vue({
   data: function () {
     return {
       name: 'hoqu',
+      bttnjur: false,
       form: {
-        appId: 'wx76c6493364c7f082',
-        appSecret: 'a10fae555e3fc83314d39f2dab8b8fbc',
+        appId: '',
+        appSecret: '',
         source: '',
         sourceCode: ''
       },
       rules: {
         appId: [
           { required: true, message: '请输入appid', trigger: 'blur' },
-          { min: 18, max: 18, message: '长度为 18个字符', trigger: 'blur' }
+          { len:18, message: '长度为 18个字符', trigger: 'blur' }
         ],
         appSecret: [
           { required: true, message: '请输入appSecret', trigger: 'blur' },
-          { min: 32, max: 32, message: '长度为 32个字符', trigger: 'blur' }
+          { len:32, message: '长度为 32个字符', trigger: 'blur' }
         ],
         sourceCode: [
-          { required: true, message: '请输入sourceCode', trigger: 'blur' },
+          { required: true, message: '请输入来源编码', trigger: 'blur' },
+          { pattern: /^[0-9a-zA-Z-]{1,8}$/, message: '仅支持数字、字母和"-"'},
+          { min: 2, max: 8, message: '长度为 2-8个字符', trigger: 'blur' }
         ],
         source: [
-          { required: true, message: '请输入source', trigger: 'blur' },
+          { required: true, message: '请输入来源简要描述', trigger: 'blur' },
+          { min: 2, max: 10, message: '长度为 2-10个字符', trigger: 'blur' }
         ],
       }
 
     }
+  },
+  mounted() {
+    this.open3()
   },
   methods: {
     dowmLoadImg() {
@@ -40,6 +47,21 @@ new Vue({
           this.form.source = ''
           this.form.sourceCode = ''
         }
+      });
+    },
+
+    open3() {
+      this.$prompt('请输入密码', '提示', {
+        confirmButtonText: '确定',
+        showClose: false,
+        showCancelButton: false,
+        closeOnClickModal: false,
+        inputPattern: /juban123/,
+        inputErrorMessage: '密码不正确'
+      }).then(({ value }) => {
+
+      }).catch(() => {
+       
       });
     }
   },
